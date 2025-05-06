87531d260b3b7350be7de6b1245c381cb5902805
Jordan Ashley <jashley6@vols.utk.edu>
2025-05-06 15:28:00 -0400

    :hammer::bug::sparkles: Resolves #4

--- doppler/doppler.ino
+++ doppler/doppler.ino
@@ -17,6 +17,8 @@ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // FFT setup
+uint16_t FFTrefresh = 50;
+
 #define MIC_PIN A0
 ArduinoFFT<double> FFT;
 const uint16_t samples = 128;
@@ -38,7 +40,7 @@ void setup() {
 }
 
 void loop() {
-  if (millis() - lastFFTTime > 500) {
+  if (millis() - lastFFTTime > FFTrefresh) {
     runFFT();
     lastFFTTime = millis();
   }
