33f14b1821f01ac3314a1027e8bca5923b9610bf
Jordan Ashley <jashley6@vols.utk.edu>
2025-05-08 15:16:15 -0400

    :memo: Update header to include circuit info for LCD

--- doppler/doppler.ino
+++ doppler/doppler.ino
@@ -1,18 +1,36 @@
 /*
+--- start of header ---
 doppler.ino
 Takes analog input from microphone
 Displays via LCD on Arduino
 
 Last edit:
-2025/05/06
+2025/05/08
 by
-Quintinne Madsen 
+Jordan Ashley @j-s-ashley
+
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+
+--- end of header ---
 */
 
 #include <arduinoFFT.h>
 #include <LiquidCrystal.h>
 
-// LCD pin setup
+// Initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
